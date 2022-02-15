52a75be63c6617ae1b0d56a694866b34329546b1
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-02-15 10:26:03 +0530

    Create 03. Students DB.sql

---
+++ 03. Students DB.sql
@@ -0,0 +1,9 @@
+-- Problem: https://www.codingninjas.com/codestudio/problems/students-data_2105460?topList=top-100-sql-problems
+
+INSERT INTO students
+VALUES
+(3,'Kim','F'),
+(4,'Molina','F'),
+(5,'Dev','M');
+
+SELECT * FROM students
