8322cbdb4de88d5f6d67384272e9940c0126e30b
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-03-07 17:49:04 +0530

    Update 12. Classes with more than 5 students.sql

--- 12. Classes with more than 5 students.sql
+++ 12. Classes with more than 5 students.sql
@@ -2,7 +2,11 @@
 
 SELECT class
 FROM
-(SELECT class, COUNT(DISTINCT student) AS num
-FROM courses
-GROUP BY class) AS temp_table
+(
+  SELECT class, 
+  COUNT(DISTINCT student) AS num
+  FROM courses
+  GROUP BY class
+) 
+AS temp_table
 WHERE num >= 5
