c440566c3f8e3d869a6f6f17d225d4537ba67865
Sahil Shimpi <65404265+Sahil-Shimpi@users.noreply.github.com>
2022-03-07 17:46:09 +0530

    Update 10. Combine Two Tables.sql

--- 10. Combine Two Tables.sql
+++ 10. Combine Two Tables.sql
@@ -1,7 +1,7 @@
 -- Problem: https://www.codingninjas.com/codestudio/problems/combine-two-tables_2110759?topList=top-100-sql-problems
 
 SELECT FirstName, LastName, City, State 
-FROM
-Person LEFT JOIN Address
+FROM Person 
+LEFT JOIN Address
 ON
 Person.PersonId = Address.PersonId
